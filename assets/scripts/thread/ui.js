10d9e5e0975a8d4a983174f8d35e6db127e2aaf8
Huy Nguyen <HuyNguyen@MacBook-Pro-cua-Le.local>
2020-09-20 18:31:36 -0400

    Commit for some small changes
    - fix the view single thread and all threads

--- assets/scripts/thread/ui.js
+++ assets/scripts/thread/ui.js
@@ -10,10 +10,9 @@ const onGetAllSuccess = function (data) {
   data.threads.forEach(thread => {
     const time = thread.createdAt.slice(0, 10)
     const line = (`
-      <li class="list-group-item">
+      <li class="list-group-item border border-info">
         <a href="#" id="${thread._id}" class="thread-title">${thread.title}</a>
-        <p class='thread-owner'>Created by ${thread.owner.email} at ${time}</p>
-        <p class='thread-content'>${thread.content}</p>
+        <p class='thread-owner'><small>Created by ${thread.owner.email} at ${time}</small></p>
       </li>
       `)
     display = line + display
@@ -29,10 +28,14 @@ const onViewTheardSuccess = function (data) {
   const thread = data.thread
   // get the topic
   let topic = (`
-    <p class='thread-title'>${thread.title}</p>
-    <p class='thread-content'>${thread.content}</p>
-    <button type="button" class="edit-thread-button" data-toggle="modal" data-target="#edit-a-thread">Edit</button>
-    <button type="button" class="delete-thread">Delete</button>
+    <li class="list-group-item border border-info">
+      <h4 class='thread-title'>${thread.title}</h4>
+    </li>
+    <li class="list-group-item border border-info">
+      <p class='thread-content'>${thread.content}</p><br>
+      <button type="button" class="edit-thread-button" data-toggle="modal" data-target="#edit-a-thread">Edit</button>
+      <button type="button" class="delete-thread">Delete</button>
+    </li>
     `)
 
   let comments = ''
@@ -40,16 +43,18 @@ const onViewTheardSuccess = function (data) {
   // get all the comments
   thread.comments.forEach(comment => {
     const line = (`
-      <p class='comment-owner'>${comment.owner.email}</p>
-      <p class='comment-content'>${comment.content}</p>
-      <button type="button" class="${comment._id}" id="edit-comment-button" data-toggle="modal" data-target="#edit-a-comment">Edit</button>
-      <button type="button" class="${comment._id}" id="delete-comment-button">Delete</button>
+      <li class="list-group-item border border-info">
+        <p class='comment-owner'>${comment.owner.email}</p>
+        <p class='comment-content'>${comment.content}</p>
+        <button type="button" class="${comment._id}" id="edit-comment-button" data-toggle="modal" data-target="#edit-a-comment">Edit</button>
+        <button type="button" class="${comment._id}" id="delete-comment-button">Delete</button>
+      </li>
       `)
     comments = comments + line
   })
 
   // put everything together before show it to the users
-  topic = '<div>' + topic + comments + '</div>'
+  topic = '<ul class="list-group">' + topic + comments + '</ul>'
   $('.single-thread').html(topic)
   $('#message').text('')
 
@@ -76,15 +81,11 @@ const onCreateThreadFailure = function () {
 }
 
 const onCreateCommentSuccess = function (data) {
-  const comment = data.thread.comments.pop()
-  console.log('comment', comment)
-  $('.single-thread').append(`
-    <p class='comment-owner'>${store.user.email}</p>
-    <p class='comment-content'>${comment.content}</p>
-    <button type="button" class="${comment._id}" id="edit-comment-button" data-toggle="modal" data-target="#edit-a-comment">Edit</button>
-    <button type="button" class="${comment._id}" id="delete-comment-button">Delete</button>
-    `)
+  api.viewThread()
+    .then(onViewTheardSuccess)
+    .catch(onViewThreadFailure)
   $('#comment-create').trigger('reset')
+
   $('#message-in-modal').text('You have successfully posted a new comment!!!!!!')
 }
 const onCreateCommentFailure = function () {
